61ca682d16fe766ad4ccb0018082e38276bb79bc
Kobyakov Sergey <s.kobyakov@hostco.ru>
2018-04-25 13:08:23 +0500

    Крестики - нолики

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Реализуй логику выбора победителя в этом файле
+function getWinner() {
+    const winner = checkWinner(0, 0, 1, 0)
+            || checkWinner(0, 1, 1, 0)
+            || checkWinner(0, 2, 1, 0)
+            || checkWinner(0, 0, 0, 1)
+            || checkWinner(1, 0, 0, 1)
+            || checkWinner(2, 0, 0, 1)
+            || checkWinner(0, 0, 1, 1)
+            || checkWinner(0, 2, 1, -1);
+
+if (!winner) {
+    if (fieldIsFull()) return 'Ничья';
+    return null;
+} else return winner;
+}
+
+function checkWinner(x, y, dx, dy) {
+    const player = getCellValue(x, y);
+    if (player === '') return false;
+
+    for (let i = 0; i < 2; i++) {
+        x += dx;
+        y += dy;
+        if (getCellValue(x, y) !== player) return false;
+    }
+
+    return player;
+}
+
+function fieldIsFull() {
+    for (let x = 0; x < 3; x++)
+        for(let y = 0; y < 3; y++)
+            if (getCellValue(x, y) === '') return false;
+
+    return true;
+}
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,4 +1,90 @@
-describe('cross-zero', () => {
-    // Этот тест можно уалить, он нужен для проверки сборки
-    it('should sum digits', () => chai.assert(1 + 1, 2));
+const cellClick = (x, y) =>
+    document.getElementById(`${x}-${y}`).parentNode.click();
+
+
+describe('getWinner', () => {
+    beforeEach(() => {
+        newGameButton.click();
+    });
+
+    it('should return null if the game is not over', () => {
+        cellClick(0, 0);
+        cellClick(0, 1);
+
+        const actual = getWinner();
+
+        chai.expect(actual).to.equal(null);
+    });
+
+    it('should return null if the field is empty', () => {
+        const actual = getWinner();
+        chai.expect(actual).to.equal(null);
+    });
+
+    it('should return `x` if first player has won in vertical case', () => {
+        cellClick(0, 0);
+        cellClick(1, 0);
+        cellClick(0, 1);
+        cellClick(1, 1);
+        cellClick(0, 2);
+        const actual = getWinner();
+        chai.expect(actual).to.equal('x');
+    });
+
+    it('should return `x` if first player has won in horizontal case', () => {
+        cellClick(0, 0);
+        cellClick(0, 1);
+        cellClick(1, 0);
+        cellClick(1, 1);
+        cellClick(2, 0);
+        const actual = getWinner();
+        chai.expect(actual).to.equal('x');
+    });
+
+    it('should return `x` if first player has won in diagonal case', () => {
+        cellClick(0, 0);
+        cellClick(1, 0);
+        cellClick(1, 1);
+        cellClick(2, 1);
+        cellClick(2, 2);
+        const actual = getWinner();
+        chai.expect(actual).to.equal('x');
+    });
+
+    it('should return `o` if second player has won in vertical case', () => {
+        cellClick(0, 0);
+        cellClick(1, 0);
+        cellClick(0, 1);
+        cellClick(1, 1);
+        cellClick(2, 2);
+        cellClick(1, 2);
+        const actual = getWinner();
+        chai.expect(actual).to.equal('o');
+    });
+
+    it('should return `o` if second player has won in horizontal case', () => {
+        cellClick(0, 1);
+        cellClick(0, 2);
+        cellClick(1, 1);
+        cellClick(1, 2);
+        cellClick(2, 0);
+        cellClick(2, 2);
+        const actual = getWinner();
+        chai.expect(actual).to.equal('o');
+    });
+
+    it('should return `o` if second player has won in diagonal case', () => {
+        cellClick(0, 1);
+        cellClick(2, 0);
+        cellClick(0, 0);
+        cellClick(1, 1);
+        cellClick(2, 2);
+        cellClick(0, 2);
+        const actual = getWinner();
+        chai.expect(actual).to.equal('o');
+    });
+
+    after(() => {
+        newGameButton.click();
+    })
 });
