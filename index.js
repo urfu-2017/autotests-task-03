36f654eec3fb2f84823008c9ddf3fc6433aa1529
Андрей Шельпяков <andreyshelpyakov@gmail.com>
2018-04-25 13:51:38 +0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Реализуй логику выбора победителя в этом файле
+function getWinner(lines) {
+	for (var i = 0; i < 3; i++) {
+		const line = lines[i];
+		if (line[0].length && line.every(c => c === line[0]))
+			return line[0];
+	};
+    for (var coloumnIndex = 0; coloumnIndex < 3; coloumnIndex++) {
+        const firstCell = lines[0][coloumnIndex];
+		for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
+            if (!firstCell.length || lines[rowIndex][coloumnIndex] !== firstCell)
+				break;
+			if (rowIndex === 2)
+			    return firstCell;
+		}
+	}
+	let firstDiag = [];
+	let secondDiag = [];
+	for (var i = 0; i < 3; i++) {
+		firstDiag.push(lines[i][i]);
+		secondDiag.push(lines[i][2 - i]);
+	}
+	if (firstDiag[0].length && firstDiag.every(c => c === firstDiag[0]))
+		return firstDiag[0];
+	if (secondDiag[0].length && secondDiag.every(c => c === secondDiag[0]))
+		return secondDiag[0];
+	return null;
+}
+
+function hasWinner(lines) {
+    if (getWinner(lines) !== null) {
+        return true;
+    }
+    const hiddenInputs = document.getElementsByTagName("input");
+    for (var i = 0; i < hiddenInputs.length; i++) {
+        if (hiddenInputs[i].value === "") {
+            return false;
+        }
+    }
+    return true;
+}
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,4 +1,52 @@
+mocha.setup('bdd');
+const cellClick = (coords) =>
+    document.getElementById(coords).parentNode.click();
+
 describe('cross-zero', () => {
-    // Этот тест можно уалить, он нужен для проверки сборки
-    it('should sum digits', () => chai.assert(1 + 1, 2));
+    var config = [
+        // по горизонтали
+        { sequence: ["0-0", "1-0", "0-1", "1-1", "0-2"], result: "x" },
+        { sequence: ["1-1", "2-2", "0-2", "2-0", "1-0", "2-1"], result: "o" },
+        // вертикали
+        { sequence: ["0-0", "1-0", "0-1", "1-1", "0-2"], result: "x" },
+        { sequence: ["0-0", "1-1", "0-2", "0-1", "1-2", "2-1"], result: "o" },
+        // по диагонали
+        { sequence: ["0-0", "0-1", "1-1", "0-2", "2-2"], result: "x" },
+        { sequence: ["2-2", "0-2", "1-2", "1-1", "2-1", "2-0"], result: "o" },
+        // ничья
+        { sequence: ["0-0", "0-1", "0-2", "2-0", "2-1", "2-2", "1-0", "1-1", "1-2"], result: "ничья" },
+        // клики по уже занятым клеткам
+        { sequence: ["0-0", "0-1", "0-0", "1-1", "0-2", "0-0", "0-1", "2-2"], result: "x" },
+        { sequence: ["0-0", "0-1", "0-2", "0-0", "0-2", "2-0", "2-1", "2-0", "2-2", "1-0", "1-1", "1-2"], result: "ничья" },
+        // незавершенная игра
+        { sequence: ["0-0"], result: "" },
+        { sequence: ["0-0", "1-1", "2-2"], result: "" },
+    ];
+    var clearButton = document.getElementById("clear");
+    var winnerElement = document.getElementById("winner");
+    beforeEach(clear);
+    after(clear);
+
+    config.forEach((_case) => {
+        it(`should be: '${_case.result}${`' for sequence: ${_case.sequence}`}`, () => {
+            initField(_case.sequence);
+            var actual = winnerMessage();
+            var expected = _case.result;
+            chai.assert.equal(actual, expected);
+        });
+    });
+
+    function clear() {
+        clearButton.dispatchEvent(new Event("click"));
+    }
+
+    function initField(sequence) {
+        for (var i = 0; i < sequence.length; i++) {
+            cellClick(sequence[i]);
+        }
+    }
+
+    function winnerMessage() {
+        return winnerElement.innerText;
+    }
 });
