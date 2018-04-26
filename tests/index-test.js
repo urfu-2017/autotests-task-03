cd781c67638207fe64ea484eda3dca50c39d241f
LazzyMind <hazipov.vlad@yandex.ru>
2018-04-26 14:21:42 +0500

    tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,4 +1,34 @@
-describe('cross-zero', () => {
-    // Этот тест можно уалить, он нужен для проверки сборки
-    it('should sum digits', () => chai.assert(1 + 1, 2));
+function clickTable(num) {
+    let cell = document.getElementsByTagName('td');
+    cell[num-1].click();
+}
+
+describe('tic tac toe', () => {
+    [
+        {
+            win: 'Крестики',
+            cells: [1,3,4,6,7],
+        },
+        {
+            win: 'Нолики',
+            cells: [4,7,5,8,1,9]
+        },
+        {
+            win: 'Крестики',
+            cells: [7,4,5,2,3],
+        },
+        {
+            win: 'Нолики',
+            cells: [4,1,7,5,8,9]
+        }        
+    ].forEach(element => {
+        it(`should print winner ${element.win} for ${element.cells}`, () => {
+            element.cells.forEach(cell => {
+                clickTable(cell);
+            });
+            let winner = document.querySelector('.winner__p').innerText;
+            document.querySelector('.button-wrapper__button').click();
+            chai.assert('Победили'+element.win.toLowerCase(), winner)
+        });
+    });
 });
