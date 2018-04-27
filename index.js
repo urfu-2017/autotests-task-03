88abced3f8696d9f0eb99abe10b313df51302c61
Irina <irihka.sedova2011@yandex.ru>
2018-04-27 22:54:07 +0500

    init

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-// Реализуй логику выбора победителя в этом файле
+const playingField = document.getElementById('playing-field');
+const playAgain = document.getElementById('play-again');
+const textByWinner = { 'x': 'Выйграл: X', 'o': 'Выйграл: O', '-': 'Ничья'};
+const classByWinner = { 'x': 'winner-x', 'o': 'winner-o', '-': 'draw'};
+let currentPlayer = 'x';
+let canChangePlayer = true;
+let winner;
+let isGameOver = false;
+
+function getCellValue(row, column) {
+    const input = document.getElementById(row + '-' + column);
+
+    return input.value;
+}
+
+playAgain.addEventListener('click', () => {
+    currentPlayer = 'x';
+    isGameOver = false;
+    playingField.setAttribute('class', 'current_' + currentPlayer);
+    document.getElementById('winner').textContent = '';
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            document.getElementById(`${i}-${j}`).removeAttribute('value');
+        }
+    }
+});
+
+playingField.addEventListener('click', event => {
+    if (!canChangePlayer) {
+        return;
+    }
+    canChangePlayer = false;
+
+    const cell = event.target;
+    const hiddenInput = cell.getElementsByTagName('input')[0];
+
+    if (!hiddenInput.value && !isGameOver) {
+        hiddenInput.setAttribute('value', currentPlayer);
+        currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+        playingField.setAttribute('class', 'current_' + currentPlayer);
+        isGameOver = getStatePlay();
+        if (isGameOver) {
+            let winnerElement = document.getElementById('winner');
+            winnerElement.textContent = textByWinner[winner];
+            winnerElement.setAttribute('class', classByWinner[winner]);
+            playingField.setAttribute('class', 'current_');
+        }
+    }
+
+    canChangePlayer = true;
+});
+
+function getStatePlay() {
+    let lines = getLines();
+
+    if (lines.indexOf('xxx') !== -1) {
+        winner = 'x';
+        return true;
+    }
+
+    if (lines.indexOf('ooo') !== -1) {
+        winner = 'o';
+        return true;
+    }
+
+    if (lines.every(line => line.length === 3)) {
+        winner = '-';
+        return true;
+    }
+
+    return false;
+}
+
+function getLines() {
+    let lines = [];
+
+    for (let i = 0; i < 3; i++) {
+        lines.push(getCellValue(i, 0) + getCellValue(i, 1) + getCellValue(i, 2));
+        lines.push(getCellValue(0, i) + getCellValue(1, i) + getCellValue(2, i));
+    }
+
+    lines.push(getCellValue(0, 0) + getCellValue(1, 1) + getCellValue(2, 2));
+    lines.push(getCellValue(0, 2) + getCellValue(1, 1) + getCellValue(2, 0));
+
+    return lines;
+}
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,4 +1,40 @@
 describe('cross-zero', () => {
-    // Этот тест можно уалить, он нужен для проверки сборки
-    it('should sum digits', () => chai.assert(1 + 1, 2));
+    const playingField = document.getElementById('playing-field');
+
+    it('Первым ходит крестик', () => {
+        chai.assert(playingField.className === 'current_x');
+    });
+
+    it('Вторым ходит нолик', () => {
+        document.getElementById('td-0-0').click();
+
+        chai.assert(playingField.className === 'current_o');
+    });
+
+    [
+        ['xxx|oo_|___', 'x'],
+        ['xo_|xo_|x__', 'x'],
+        ['xo_|ox_|__x', 'x'],
+        ['ooo|xx_|x__', 'o'],
+        ['ox_|ox_|o_x', 'o'],
+        ['ox_|xo_|x_o', 'o'],
+        ['oxx|xoo|xox', '-'],
+        ['xox|xox|oxo', '-'],
+    ].forEach(test => {
+        it(`Итог игры: ${test[1]}, если поле == ${test[0]}`, () => {
+            const field = test[0].split('|').map(line => line.split(''));
+
+            field.map((line, i) => line.map((cell, j) => {
+                if (cell !== '_') {
+                    document.getElementById(`${i}-${j}`).setAttribute('value', cell);
+                }
+            }));
+
+            getStatePlay();
+
+            chai.assert(winner === test[1]);
+        })
+    });
+
+    afterEach(() => document.getElementById('play-again').click());
 });
