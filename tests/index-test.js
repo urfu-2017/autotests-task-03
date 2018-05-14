f0ffe129552e24687952268900ff3f6c43496f5a
Мишакова Наталья Сергеевна <mishakova.n@mail.ru>
2018-05-14 21:17:03 +0500

    Update index-test.js

--- tests/index-test.js
+++ tests/index-test.js
@@ -11,21 +11,40 @@ describe('cross-zero', () => {
 	beforeEach(() => {
         button.click();
     });
-	it('should sum digits', () => chai.assert(1 + 1, 2));
 	it('should say first player win', () => {
-	fillField(new Array(0,2,1,1,1,2,0,0,2,2));
-	chai.assert.equal(result.innerHTML, "Первый игрок победил");
+		fillField(new Array(0,2,1,1,1,2,0,0,2,2));
+		chai.assert.equal(result.innerHTML, "Первый игрок победил");
 	});
 	it('should say second player win', () => {
-	fillField(new Array(0,0,2,1,1,0,2,0,0,2,2,2));
-	chai.assert.equal(result.innerHTML, "Второй игрок победил");
+		fillField(new Array(0,0,2,1,1,0,2,0,0,2,2,2));
+		chai.assert.equal(result.innerHTML, "Второй игрок победил");
 	});
 	it('should say no winner', () => {
-	fillField(new Array(0,0,0,1,0,2,2,2,2,1,2,0,1,0,1,1,1,2));
-	chai.assert.equal(result.innerHTML, "Ничья");
+		fillField(new Array(0,0,0,1,0,2,2,2,2,1,2,0,1,0,1,1,1,2));
+		chai.assert.equal(result.innerHTML, "Ничья");
 	});
 	it ('should not write result in new game',() => {
 		chai.assert.equal(result.innerHTML, "Сделайте ход");
+	});
+	it('should be no errors when clicking on the cell again', () => {
+		fillField(new Array(0,0,0,0,0,1,0,1,0,2,0,2,1,2,1,1,2,0,1,0,2,2,2,1));
+		chai.assert.equal(result.innerHTML, "Ничья");
+	});
+	it('should determine the winner with a horizontal victory', () => {
+		fillField(new Array(1,1,0,0,1,0,0,1,1,2));
+		chai.assert.equal(result.innerHTML, "Первый игрок победил");
+	});
+	it('should determine the winner with a vertical victory', () => {
+		fillField(new Array(1,1,0,0,0,1,1,0,2,1));
+		chai.assert.equal(result.innerHTML, "Первый игрок победил");
+	});
+	it('should determine the winner with a diagonal victory', () => {
+		fillField(new Array(1,1,0,1,0,0,0,2,2,2));
+		chai.assert.equal(result.innerHTML, "Первый игрок победил");
+	});
+	it('should be zero after cross', () => {
+		fillField(new Array(0,0,1,0));
+		chai.assert.equal(getCellValue(1,0), 'o');
 	});
 });
 
