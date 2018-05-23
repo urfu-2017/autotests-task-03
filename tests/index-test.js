1844869b61f94d311f23a97f63454f62dd392c44
grobik3 <nikita.buravko@gmail.com>
2018-05-23 14:44:15 +0500

    Добавил кнопку и в тестах использую ее вместо функции обновить

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,12 +1,12 @@
 const push = (x, y) => 
 document.getElementById(`${x}-${y}`).parentNode.click(); 
-
+const cliclOnButton = document.getElementById("refresh");
 describe('cross-first', () => { 
     // Этот тест можно уалить, он нужен для проверки сборки 
     it('should sum digits', () => chai.assert(1 + 1, 2)); 
-    cleargame(); 
+    cliclOnButton.click();
     it('row win x, should alert x wins', () => { 
-        cleargame(); 
+        cliclOnButton.click();
         push(0, 0); 
         push(1, 0); 
         push(0, 1); 
@@ -14,10 +14,10 @@ describe('cross-first', () => {
         push(0, 2); 
         const actual = counter(); 
         chai.expect(actual).to.equal(1);
-        cleargame(); 
+        cliclOnButton.click();
     }); 
     it('zigzag win x, should alert x wins', () => { 
-        cleargame(); 
+        cliclOnButton.click(); 
         push(0, 0); 
         push(1, 0); 
         push(1, 1); 
@@ -25,10 +25,10 @@ describe('cross-first', () => {
         push(2, 2); 
         const actual = counter(); 
         chai.expect(actual).to.equal(1);
-        cleargame(); 
+        cliclOnButton.click(); 
     }); 
     it('column win x, should alert x wins', () => { 
-        cleargame(); 
+        cliclOnButton.click();
         push(0, 0); 
         push(0, 1); 
         push(1, 0); 
@@ -36,10 +36,10 @@ describe('cross-first', () => {
         push(2, 0); 
         const actual = counter(); 
         chai.expect(actual).to.equal(1);
-        cleargame(); 
+        cliclOnButton.click(); 
     }); 
     it('-zigzag win x, should alert x wins', () => { 
-        cleargame(); 
+        cliclOnButton.click(); 
         push(0, 2); 
         push(0, 1); 
         push(1, 1); 
@@ -47,13 +47,27 @@ describe('cross-first', () => {
         push(2, 0); 
         const actual = counter(); 
         chai.expect(actual).to.equal(1);
-        cleargame(); 
+        cliclOnButton.click();
     });
-    
+    it('x win, if did double click on element', () => { 
+        cliclOnButton.click();
+        push(0, 2); 
+        push(0, 2);
+        push(0, 1); 
+        push(0, 1); 
+        push(1, 1); 
+        push(1, 1); 
+        push(2, 1); 
+        push(2, 1); 
+        push(2, 0); 
+        const actual = counter(); 
+        chai.expect(actual).to.equal(1);
+        cliclOnButton.click(); 
+    }); 
 });
 describe('cross-second', () => { 
     it('row win o, should alert o wins', () => { 
-        cleargame(); 
+        cliclOnButton.click();
         push(0, 0); 
         push(1, 0); 
         push(0, 1); 
@@ -62,10 +76,10 @@ describe('cross-second', () => {
         push(1, 2); 
         const actual = counter(); 
         chai.expect(actual).to.equal(2);
-        cleargame(); 
+        cliclOnButton.click();
     }); 
     it('zigzag win o, should alert o wins', () => { 
-        cleargame(); 
+        cliclOnButton.click();
         push(0, 1); 
         push(0, 0); 
         push(1, 0); 
@@ -74,10 +88,10 @@ describe('cross-second', () => {
         push(2, 2); 
         const actual = counter(); 
         chai.expect(actual).to.equal(2);
-        cleargame(); 
+        cliclOnButton.click(); 
     }); 
     it('column win o, should alert o wins', () => { 
-        cleargame(); 
+        cliclOnButton.click(); 
         push(0, 1); 
         push(0, 0); 
         push(1, 1); 
@@ -86,10 +100,10 @@ describe('cross-second', () => {
         push(1, 0);
         const actual = counter(); 
         chai.expect(actual).to.equal(2);
-        cleargame(); 
+        cliclOnButton.click();
     }); 
     it('-zigzag win o, should alert o wins', () => { 
-        cleargame(); 
+        cliclOnButton.click();
         push(0, 1); 
         push(0, 2); 
         push(2, 1); 
@@ -98,12 +112,12 @@ describe('cross-second', () => {
         push(2, 0); 
         const actual = counter(); 
         chai.expect(actual).to.equal(2);
-        cleargame(); 
+        cliclOnButton.click();
     }); 
 });
 describe('cross-third', () => { 
     it('no winers', () => { 
-        cleargame(); 
+        cliclOnButton.click(); 
         push(0, 0); 
         push(0, 1); 
         push(1, 0); 
@@ -115,6 +129,25 @@ describe('cross-third', () => {
         push(2, 2);
         const actual = counter(); 
         chai.expect(actual).to.equal(0);
-        cleargame(); 
-    }); 
+        cliclOnButton.click(); 
+    })
+});
+describe('check button clearGame', () => { 
+    it('o win, when in the middle of the game press the restart button ', () => { 
+        cliclOnButton.click();
+        push(0, 2); 
+        push(0, 1); 
+        push(1, 1); 
+        push(2, 1); 
+        cliclOnButton.click();
+        push(0, 1); 
+        push(0, 2); 
+        push(2, 1); 
+        push(1, 1); 
+        push(2, 2); 
+        push(2, 0);
+        const actual = counter(); 
+        chai.expect(actual).to.equal(2);
+        cliclOnButton.click();
+    })
 });
